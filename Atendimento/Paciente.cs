47a8ad07906cdbd1e93369dc74102f3f41407df5
Matheus Ferreira <matheus.ferreira359@etec.sp.gov.br>
2022-05-04 17:25:33 -0300

    Primeiro commit

---
+++ Atendimento/Paciente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atendimento
+{
+    class Paciente
+    {
+        public string nome;
+        public string idade;
+        public string cpf;
+        public string endereco;
+        public string preferencia = "s";
+           
+    }
+}
--- Atendimento/Program.cs
+++ Atendimento/Program.cs
@@ -7,9 +7,32 @@ using System.Threading.Tasks;
 namespace Atendimento
 {
     class Program
-    {
-        static void Main(string[] args)
+    {float
+        static Atendimento void Main(string[] args)
         {
+         int[] vetor = new int[5];         
+         Console.WriteLine("Controle de entrada de pacientes");
+         Paciente[] Lista = new Paciente[10];                 
+      
+            for (int i=0; i< 10; i++)
+            {
+                Lista[i] = new Paciente();
+                Console.WriteLine("Digite o nome do Paciente");
+                Lista [i].nome = Console.ReadLine();
+                Console.WriteLine("Digite a idade do paciente");
+                Lista[i].idade = Console.ReadLine();
+                Console.WriteLine("Digite o CPF do paciente");
+                Lista[i].cpf = Console.ReadLine();
+                Console.WriteLine("O paciente é preferencial? s/n?");
+                if ("s" != "n")
+                {
+                    Lista[i].preferencia = Console.ReadLine();
+                }
+                Console.ReadKey();
+            }
+           
+            
         }
+         
     }
 }
